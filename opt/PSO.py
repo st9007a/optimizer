352cf179a037518d6d4c18063f9d411a42e6405c
uno <st9007a@gmail.com>
2018-05-07 23:57:35 +0800

    Fix bug when update gbest

--- opt/PSO.py
+++ opt/PSO.py
@@ -22,20 +22,18 @@ class StandardPSO():
         self.particles = np.random.normal(mean, std, [self.num_particles, bench.dims])
 
         # truncated
-        self.particles = np.where(self.particles >= bench.up, mean, self.particles)
-        self.particles = np.where(self.particles <= bench.low, mean, self.particles)
+        np.clip(self.particles, bench.low, bench.up, out = self.particles)
 
         self.speed = np.random.normal(0, 1, [self.num_particles, bench.dims])
 
         self.pbest_val = np.zeros([self.num_particles])
         self.pbest_val.fill(inf)
 
-        self.pbest_vec = np.zeros([self.num_particles, bench.dims])
+        self.pbest_vec = [None] * self.num_particles
 
         self.gbest_val = inf
-        self.gbest_vec = np.zeros([bench.dims])
+        self.gbest_vec = None
 
-        self.compute(bench)
 
     def compute(self, bench):
         for idx, vec in enumerate(self.particles):
@@ -43,15 +41,11 @@ class StandardPSO():
 
             if score < self.pbest_val[idx]:
                 self.pbest_val[idx] = score
-                self.pbest_vec[idx] = vec
+                self.pbest_vec[idx] = list(vec)
 
             if score < self.gbest_val:
                 self.gbest_val = score
-                self.gbest_vec = vec
-
-    def clip(self, bench):
-        self.particles = np.where(self.particles >= bench.up, bench.up - 0.0001, self.particles)
-        self.particles = np.where(self.particles <= bench.low, bench.low + 0.0001, self.particles)
+                self.gbest_vec = list(vec)
 
     def compute_speed(self, speed, idx, iters, r1, r2):
         return speed + self.c1 * r1 * (self.pbest_vec[idx] - self.particles[idx]) + self.c2 * r2 * (self.gbest_vec - self.particles[idx])
@@ -61,15 +55,17 @@ class StandardPSO():
 
     def step(self, iters, bench):
 
+        self.compute(bench)
+
         for idx, speed in enumerate(self.speed):
             r1 = uniform(0, 1)
             r2 = uniform(0, 1)
             self.speed[idx] = self.compute_speed(speed, idx, iters, r1, r2)
+
+        for idx in range(self.num_particles):
             self.particles[idx] += self.speed[idx]
 
-            self.clip(bench)
-
-        self.compute(bench)
+        np.clip(self.particles, bench.low, bench.up, out = self.particles)
 
     def optimize(self, bench, iters):
 
