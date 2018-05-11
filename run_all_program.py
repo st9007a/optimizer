0515b0cb416a46f49d4555ca4e7327658f0309fe
uno <st9007a@gmail.com>
2018-05-11 11:12:12 +0800

    Run all program

---
+++ run_all_program.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import pickle
+import numpy as np
+from datetime import datetime
+from pprint import pprint
+
+from opt.GSA import GSA
+from opt.PSO import StandardPSO
+from bench import *
+
+def run_program(optimizer, bench, iters, path):
+    mean_fitness_last_iter = []
+    history_100_rounds = []
+
+    for i in range(100):
+        optimizer.optimize(bench = bench, iters = iters)
+
+        history_100_rounds.append(optimizer.history)
+        mean_fitness_last_iter.append(optimizer.mean_fitness_last_iter(bench))
+
+    res = {
+        'history': history_100_rounds,
+        'mean_fitness_last_iter': mean_fitness_last_iter,
+    }
+
+    with open(path, 'wb') as p:
+        pickle.dump(res, p, protocol = pickle.HIGHEST_PROTOCOL)
+
+    print('Done, save result to ' + path)
+
+if __name__ == '__main__':
+
+    benchs = [func1, func2, func3, func4, func5, func6, func7, func8]
+    PSOOptimizer = StandardPSO(c1 = 0.5, c2 = 0.5, num_particles = 50)
+    GSAOptimizer = GSA(num_agents = 50, g = 9.8, kbest = 40)
+
+    for idx, b in enumerate(benchs):
+        run_program(PSOOptimizer, b, 1000, 'results/PSO_func%d.pkl' % (idx + 1))
+        run_program(GSAOptimizer, b, 1000, 'results/GSA_func%d.pkl' % (idx + 1))
